8cb674ff915416123ed5063e74a7735a6966aaae
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2022-02-25 15:19:30 +0530

    e

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect,useState } from 'react';
+import { useEffect} from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import TvIcon from '@mui/icons-material/Tv';
